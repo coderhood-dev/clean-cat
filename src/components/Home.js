b732ca2814a3af84d4349d358fcb4f024ecc0d39
Cesar Cordoba <cordobacesar29@gmail.com>
2020-12-04 21:42:33 -0300

    Merge branch 'main' of https://github.com/coderhood-dev/clean-cat into main

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,23 +1,21 @@
-import { Link } from "react-router-dom";
+import { Link } from "@chakra-ui/react";
 import catLogo from "../assets/cat-clean-256.png";
 
 export const Home = () => {
   return (
     <div className="App">
-        <header className="App-header">
-          <img src={catLogo} alt="cat logo" />
-          <p>
-            Clean Cat
-          </p>
-          <Link 
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </Link>
-        </header>
-      </div>
+      <header className="App-header">
+        <img src={catLogo} alt="cat logo" />
+        <p>Clean Cat</p>
+        <Link
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </Link>
+      </header>
+    </div>
   );
 };
