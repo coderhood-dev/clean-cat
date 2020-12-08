19cf76d599812fe23ccb6536597432d5b6a1a3a6
Agustin-Mediotti <agustinmediotti@gmail.com>
2020-12-08 01:33:45 -0300

    Fetch data from API url

---
+++ src/components/Api.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from "react";
+import { Box, Image } from "@chakra-ui/react";
+import Loading from "./Loading";
+
+const url = "https://api.thecatapi.com/v1/images/search";
+
+function Api() {
+  const [loading, setLoading] = useState(true);
+  const [images, setImages] = useState([]);
+
+  useEffect(() => {
+    fetchImage();
+  }, []);
+
+  const fetchImage = async () => {
+    setLoading(true);
+    try {
+      const response = await fetch(url);
+      const images = await response.json();
+      setImages(images);
+      console.log(images);
+      setLoading(false);
+    } catch (e) {
+      console.log(e);
+      setLoading(false);
+    }
+  };
+
+  if (loading) {
+    return <Loading />;
+  }
+
+  return (
+    <Box>
+      <Image src={images[0].url} alt="kitty" />
+    </Box>
+  );
+}
+
+export default Api;
